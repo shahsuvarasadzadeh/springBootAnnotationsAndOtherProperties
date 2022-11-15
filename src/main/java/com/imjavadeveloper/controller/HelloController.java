402a57b3184aa33fb423cf077568b53acc9d58a8
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2022-11-15 19:33:58 +0400

    Annotations

--- src/main/java/com/imjavadeveloper/controller/HelloController.java
+++ src/main/java/com/imjavadeveloper/controller/HelloController.java
@@ -1,17 +1,44 @@
 package com.imjavadeveloper.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.imjavadeveloper.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class HelloController {
 
-    @RequestMapping(path = "/hello",method = RequestMethod.GET)
-    public String sayHello(){
+    @GetMapping(value = "/hello")
+    public String sayHello() {
         return "Hello  Taceddin!";
     }
+
+
+    @GetMapping("/hello/{m}")
+    public String returnMessage(@PathVariable("m") String m) {
+
+        return "Your message : "+m;
+
+    }
+
+    @GetMapping("/message1")
+        public String getMyMessage(@RequestParam(name = "message",required = false,defaultValue = "My Words") String message1){
+       return "Your Message : "+message1;
+        }
+
+
+        @GetMapping({"/message2","/message2/{message}"})
+    public String getMyMessage1(@PathVariable(name = "message",required = false)String message){
+        return  "Your Message : "+message;
+        }
+
+        @PostMapping("user")
+    public List<User> saveAllUsers(@RequestBody List<User> users){
+            System.out.println("User saved!");
+        users.forEach(user -> user.setPassword("") );
+           return users;
+        }
 }
